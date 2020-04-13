6a2d832f61844010b8c9138485c40a9b9a603029
datquocngo <shaco1409199@gmail.com>
2020-04-13 16:30:19 +0700

    change method to get product by name and by category

--- src/main/java/com/example/hopeshop/controller/user/ProductController.java
+++ src/main/java/com/example/hopeshop/controller/user/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,9 +38,8 @@ public class ProductController {
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/productByName", method = RequestMethod.GET)
-    public ResponseEntity<List<Product>> productByName(@RequestBody Map<String, String> map) {
-        String name = map.get("productName");
+    @RequestMapping(value = "/productByName/{productName}", method = RequestMethod.GET)
+    public ResponseEntity<List<Product>> productByName(@PathVariable("productName") String name) {
         List<Product> products = productService.findAllByNameIsContaining(name);
         if (products.isEmpty()) {
             return new ResponseEntity<List<Product>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
@@ -47,8 +47,9 @@ public class ProductController {
         return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/productByCategory", method = RequestMethod.GET)
-    public ResponseEntity<List<Product>> productByCategory(@RequestBody Category category) {
+    @RequestMapping(value = "/productByCategory/{categoryId}", method = RequestMethod.GET)
+    public ResponseEntity<List<Product>> productByCategory(@PathVariable("categoryId") int categoryId) {
+        Category category = categoryService.findById(categoryId);
         List<Product> products = productService.findAllByCategory(category);
         if (products.isEmpty()) {
             return new ResponseEntity<List<Product>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
